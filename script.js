8f0cba9e9f31e032d29023ea40fcb32227b064f3
Devyd Silva <devyd.santana20201@gmail.com>
2025-05-15 17:08:50 -0300

    telefone

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-const nome = document.getElementById("nome");
-const mensagem = document.getElementById("Mensagem");
-const telefone = '5581995566408';
+function enviarWhats(event){
 
-const texto = `Olá, Me chamo ${nome.value} e gostaria de saber mais sobre o serviço. Mensagem: ${mensagem.value}`;
-const msgFormatada = encodeURIComponent(texto);
+    event.preventDefault(); // Impede o envio do formulário padrão
 
-const url = `https://wa.me/qr/${telefone}/?t=${msgFormatada}`;
+    const nome = document.getElementById("nome");
+    const mensagem = document.getElementById("Mensagem");
+    const telefone = '5581995566408';
 
-window.open(url, '_blank');
+    const texto = `Olá, Me chamo ${nome.value} e gostaria de saber mais sobre o serviço. Mensagem: ${mensagem.value}`;
+    const msgFormatada = encodeURIComponent(texto);
+
+    const url = `https://wa.me/qr/${telefone}/?t=${msgFormatada}`;
+
+    window.open(url, '_blank');
+}
+
+
