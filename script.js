05abb9ab06097e250ef752261ef4f748c52f0e8a
Devyd Silva <devyd.santana20201@gmail.com>
2025-05-15 01:11:18 -0300

    script

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const nome = document.getElementById("nome");
-const mensagem = document.getElementById("mensagem");
+const mensagem = document.getElementById("Mensagem");
 const telefone = '5581995566408';
 
-const texto = `Olá, Me chamo ${nome} e gostaria de saber mais sobre o serviço. Mensagem: ${mensagem.value}`;
+const texto = `Olá, Me chamo ${nome.value} e gostaria de saber mais sobre o serviço. Mensagem: ${mensagem.value}`;
 const msgFormatada = encodeURIComponent(texto);
 
 const url = `https://wa.me/qr/${telefone}/?t=${msgFormatada}`;
